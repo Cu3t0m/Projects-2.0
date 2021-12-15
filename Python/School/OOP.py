e5eff56fcbc633880fd3d13964073a25ba745dc6
Kishore S <k.sath214@gmail.com>
2021-12-15 21:34:09 +0000

    Updates

--- Python/School/OOP.py
+++ Python/School/OOP.py
@@ -2,8 +2,11 @@
 
 # Object Oriented Programming.py
 
+# Imports
+import random
+
 # Defining a class, this will be used later
-class pokemon():
+class player_Pokemon():
   def __init__(self,name="",strength="",hp="",maxHP="",level=""):
     self.name = name
     self.strength = strength
@@ -18,10 +21,41 @@ class pokemon():
     print("Strength: ",self.strength)
     print("HP: ",self.hp,"/",self.maxHP)
 
-playerPokemon = pokemon()
+class enemy_Pokemon():
+    def __init__(self,ename="",estrength="",ehp="",emaxHP="",elevel=""):
+        self.ename = ename
+        self.estrength = estrength
+        self.ehp = ehp
+        self.emaxHP = emaxHP
+        self.elevel = elevel
+    
+    def enemy_profile(self):
+        print("Your enemy:\n")
+        print("Name: ",self.ename)
+        print("Level: ",self.elevel)
+        print("Strength: ",self.estrength)
+        print("HP: ",self.ehp,"/",self.emaxHP)
+
+# Defining functions
+def RNG(x,y):
+  damageMultiplier = random.randint(0,10)
+  return damageMultiplier
+
+
+# Defines the player and enemy's properties, like health, strength, level, etc. and prints out a profile
+
+playerPokemon = player_Pokemon()
 playerPokemon.name = "Pikachu"
-playerPokemon.strength = 10
+playerPokemon.strength = 9
 playerPokemon.hp = 40
 playerPokemon.maxHP = 40
 playerPokemon.level = 5
 playerPokemon.character_profile()
+
+enemyPokemon = enemy_Pokemon()
+enemyPokemon.ename = "Charmander"
+enemyPokemon.estrength = 5
+enemyPokemon.ehp = 40
+enemyPokemon.emaxHP = 40
+enemyPokemon.elevel = 5
+enemyPokemon.enemy_profile()
