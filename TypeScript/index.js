f5f24dd3d3532297277cf2109ebf23d1a355e82d
Kishore S <k.sath214@gmail.com>
2022-02-12 17:40:09 +0000

    Cleanup 2.0

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,9 +1,57 @@
 #!/usr/bin/env node
-// let message: string = "Hello, World!";
-var message = "Hello, this is TypeScript!";
-console.log(message);
-// Create new h1
-var heading = document.createElement('h1');
-heading.textContent = message;
-// Append the heading
-document.body.appendChild(heading);
+function getProduct(id) {
+    return {
+        id: id,
+        name: "Awesome Gadget ".concat(id),
+        price: 99.5
+    };
+}
+// console.log(`The product ${product.name} costs $${product.price}`);
+var product = getProduct(1);
+var showProduct = function (name, price) {
+    console.log("The product ".concat(name, " costs ").concat(price, "."));
+};
+showProduct(product.name, product.price);
+console.log('Hello'.length);
+console.log('Hello'.toLocaleUpperCase());
+// Types:
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// const heading = document.querySelector('h1');
+// Type annotations
+// let count: number;
+// count = 'Hello';
+var count = 1;
+var uname = 'John';
+var age = 25;
+var active = true;
+var names = ['John', 'Jane', 'Doe'];
+var person;
+person = {
+    name: 'John',
+    age: 25
+};
+var greeting;
+greeting = function (name) {
+    return "Hi ".concat(name);
+};
+var counter = 0;
+function setCounter(max) {
+    if (max === void 0) { max = 100; }
+    return counter++;
+}
+var items = [1, 2, 3, null];
+// let items = [0, 1, null, 'Hi'];
+var arr = [new Date(), new RegExp('\d+')];
+document.addEventListener('click', function (event) {
+    console.log(event.button);
+});
+// document.addEventListener('scroll', function (event) {
+//     console.log(event.button);
+// })
+
+// =>
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,39 +1,134 @@
 #!/usr/bin/env node
+interface Product {
+    id: number,
+    name: string,
+    price: number,
+}
+  
+function getProduct(id: number): Product {
+    return {
+      id: id,
+      name: `Awesome Gadget ${id}`,
+      price: 99.5,
+    }
+}
+  
+// console.log(`The product ${product.name} costs $${product.price}`);
+  
+const product = getProduct(1);
+const showProduct = (name: string, price: number) => {
+  console.log(`The product ${name} costs ${price}.`);
+}
+  
+showProduct(product.name, product.price)
+console.log('Hello'.length)
+console.log('Hello'.toLocaleUpperCase())
+  
+// Types:
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+  
+// const heading = document.querySelector('h1');
 
-// let message: string = "Hello, World!";
-let message: string = "Hello, this is TypeScript!";
-console.log(message);
+// Type annotations
 
-// Create new h1
-let heading = document.createElement('h1');
-heading.textContent = message;
 
-// Append the heading
-document.body.appendChild(heading);
+// let count: number;
+// count = 'Hello';
 
-// JS Scheiße
-// let box;
-// console.log(typeof (box));
+let count: number = 1;
 
-// box = "Hello";
-// console.log(typeof (box));
+let uname: string = 'John';
+let age: number = 25;
+let active: boolean = true;
 
-// box = 100;
-// console.log(typeof (box));
+let names: string[] = ['John', 'Jane', 'Doe']
 
-// function getProductId(id: any) {
-//   return {
-//     id: id,
-//     name: `Awesome Gadget ${id}`,
-//     price: 99.5,
-//   }
+let person: {
+    name: string;
+    age: number
+};
+
+person = {
+    name: 'John',
+    age: 25
+}
+
+let greeting: (name: string) => string;
+
+greeting = function (name: string) {
+    return `Hi ${name}`;
+}
+
+// let counter: number = 0;
+
+// function setCounter(max=100) {
+//     return counter++;
 // }
 
-// const product = getProductId(1);
-// console.log(`The Product ${product.name} costs $${product.price}`)
-//
-// const showProduct = (name: any, price: any) => {
-//   console.log(`The product ${name} costs ${price}$.`)
-// }
+let items = [1, 2, 3, null];
 
-// showProduct(product.price, product.name)
+// let items = [0, 1, null, 'Hi'];
+
+let arr = [new Date(), new RegExp('\d+')];
+
+// document.addEventListener('click', function(event) {
+//     console.log(event.button);
+// });
+
+// document.addEventListener('scroll', function (event) {
+//     console.log(event.button);
+// })
+
+let price = 9.95;
+
+let counter: number = 0;
+let x: number = 100,
+    y: number = 200;
+
+let bin = 0b100;
+let diffBin: number = 0B010;
+
+console.log(bin)
+console.log(diffBin)
+
+let octal: number = 0o10;
+let hexadec: number = 0XA;
+
+let chonkInt: bigint = 9007199254740991n;
+
+let fName: string = 'John';
+let jobTitle: string = 'Web Dev';
+
+let desc = `This TS string can
+span multiple
+lines
+of
+text
+`;
+
+let profile: string = `I'm ${fName} and I'm a ${jobTitle}`;
+
+console.log(profile)
+
+let pend: boolean;
+pend = true;
+
+
+pend = false;
+
+
+let employee: object;
+
+employee = {
+    fName: 'John',
+    lName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee)
